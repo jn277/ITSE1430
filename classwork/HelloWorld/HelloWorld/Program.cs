694f7a81c011fecf7b4145dc46e91036491c47a5
jn277 <donald.helaire@my.tccd.edu>
2020-08-26 20:55:01 -0500

    Debugger

--- classwork/HelloWorld/HelloWorld/Program.cs
+++ classwork/HelloWorld/HelloWorld/Program.cs
@@ -7,6 +7,12 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            int hours = 40;
+            double payRate = 12.80;
+
+            double totalPay = hours * payRate;
+
         }
     }
 }
