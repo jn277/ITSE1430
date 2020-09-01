6f576d38dfacbc95eecc28e208784497e25aa19f
jn277 <donald.helaire@my.tccd.edu>
2020-08-31 21:31:20 -0500

    MovieLibrary declarations

---
+++ classwork/MovieLibrary/MovieLibrary/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MovieLibrary
+{
+    class Program
+    {
+        static void Main ( string[] args )
+        {
+            //FunWithTypes();
+            //FunWithVariables();
+
+            string title = "";
+            string description = "";
+            string rating = "";
+        }
+
+    private static void FunWithVariables()
+    {
+        int hours = 10;
+        int value;
+        value = 10;
+        int calculatedValue = value * 10;
+    }
+     static void FunWithTypes()
+        {
+            bool isPassing = true;
+            char letterA = 'A';
+            string name = "Bob";
+        }
+    }
+}
