a04cbbb06185ad6089e32b3c84fd5921fa8fd14f
jn277 <donald.helaire@my.tccd.edu>
2020-09-01 15:44:31 -0500

    Lab1_Budget

---
+++ labs/Budget/Budget/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * Donald Helaire
+ * ITSE 1430 
+ * Lab 1 - Budget
+ */
+using System;
+
+namespace Budget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
