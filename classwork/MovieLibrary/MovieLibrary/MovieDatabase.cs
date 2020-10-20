04e598918c108a418750a8a8bb6d4ab464569dfa
jn277 <donald.helaire@my.tccd.edu>
2020-10-19 21:30:08 -0500

    MovieLibrary 10192020

--- classwork/MovieLibrary/MovieLibrary/MovieDatabase.cs
+++ classwork/MovieLibrary/MovieLibrary/MovieDatabase.cs
@@ -9,33 +9,84 @@ namespace MovieLibrary
         //Default constructor to seed database
         public MovieDatabase ()
         {
+            //Collection initializer syntax
+            //var items = new Movie[] {
+                //new Movie() {
+                    //Name = "Jaws",
+                    //ReleaseYear = 1977,
+                    //RunLength = 190,
+                    //Description = "Shark movie",
+                    //IsClassic = true,
+                    //Rating = "PG",  // Can have a comma at end
+                //},
+                //new Movie() {
+                    //Name = "Jaws 2",
+                    //ReleaseYear = 1979,
+                    //RunLength = 195,
+                    //Description = "Shark movie",
+                    //IsClassic = true,
+                    //Rating = "PG-13",
+                //},
+                //new Movie() {
+                    //Name = "Dune",
+                    //ReleaseYear = 1985,
+                    //RunLength = 220,
+                    //Description = "Based on book",
+                    //IsClassic = true,
+                    //Rating = "PG",
+                //}
+            //};
+            //foreach (var item in items)
+                //Add(item, out var error);
+            //Not needed here - clears all items from list
+            //_movies.Clear();
 
             //Seed database
-            var movie = new Movie();
-            movie.Name = "Jaws";
-            movie.ReleaseYear = 1977;
-            movie.RunLength = 190;
-            movie.Description = "Shark movie";
-            movie.IsClassic = true;
-            movie.Rating = "PG";
+            // Object initializer - only usable on new operator
+            //   Limitations:
+            //      1. Can only set properties with setters
+            //      2. Can set properties that are themselves new'ed up but cannot set child properties
+            //                    Position = new Position() { Name = "Boss" };  //Allowed
+            //                    Position.Title = "Boss";  //Not allowed
+            //      3. Properties cannot rely on other properties
+            //               Description = "blah",
+            //               FullDescription = Description
+            //var movie = new Movie();
+            //movie.Name = "Jaws";
+            //movie.ReleaseYear = 1977;
+            //movie.RunLength = 190;
+            //movie.Description = "Shark movie";
+            //movie.IsClassic = true;
+            //movie.Rating = "PG";
+            var movie = new Movie() {
+                Name = "Jaws",
+                ReleaseYear = 1977,
+                RunLength = 190,
+                Description = "Shark movie",
+                IsClassic = true,
+                Rating = "PG",
+            };
+
             Add(movie, out var error);
 
-            movie = new Movie();
-            movie.Name = "Jaws 2";
-            movie.ReleaseYear = 1979;
-            movie.RunLength = 195;
-            movie.Description = "Shark movie";
-            movie.IsClassic = true;
-            movie.Rating = "PG-13";
+            movie = new Movie() {
+                Name = "Jaws 2",
+                ReleaseYear = 1979,
+                RunLength = 195,
+                Description = "Shark movie",
+                IsClassic = true,
+                Rating = "PG-13",
+            };
             Add(movie, out error);
 
-            movie = new Movie();
-            movie.Name = "Dune";
-            movie.ReleaseYear = 1985;
-            movie.RunLength = 220;
-            movie.Description = "Based on book";
-            movie.IsClassic = true;
-            movie.Rating = "PG";
+            movie = new Movie() {
+                Name = "Dune",
+                ReleaseYear = 1985,
+                RunLength = 220,
+                Description = "Based on book",
+                IsClassic = true,
+                Rating = "PG",
+            };
             Add(movie, out error);
 
         }
