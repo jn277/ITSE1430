e879da74a776e0292f89cf0c65bfa7e44124dfa5
jn277 <donald.helaire@my.tccd.edu>
2020-12-03 17:16:23 -0600

    CharacterModel.Web Lab5

---
+++ labs/Lab5/CharacterRoster.Web/Controllers/CharacterController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Configuration;
+using System.Web.Mvc;
+
+using CharacterRoster.Web.Models;
+
+
+namespace CharacterRoster.Web.Controllers
+{
+    public class CharacterController : Controller
+    {
+        public CharacterController ()
+        {
+            //Gets the CharacterDatabase connection string from the config file
+            var connString = ConfigurationManager.ConnectionStrings["MemoryCharacterDatabase"].ConnectionString;
+
+           //var _database = new MemoryCharacterDatabase.MemoryCharacterDatabase(connString);
+        }
+
+        // GET: Character
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
