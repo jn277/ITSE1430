1bb3562b6e4e0cef53478c0abb8244b4a0c7e893
jn277 <donald.helaire@my.tccd.edu>
2020-12-05 14:33:55 -0600

    CharacterRoster.Web Lab5

--- labs/Lab5/CharacterRoster.Web/Controllers/CharacterController.cs
+++ labs/Lab5/CharacterRoster.Web/Controllers/CharacterController.cs
@@ -1,10 +1,18 @@
-﻿using System;
+﻿/*
+ * ITSE 1430
+ * Donald Helaire
+ * Lab5
+ */
+
+using System;
 using System.Configuration;
 using System.Web.Mvc;
-
+using CharacterCreator.Memory;
 using CharacterRoster.Web.Models;
 
-
+namespace CharacterCreator.Memory 
+{ 
+}
 namespace CharacterRoster.Web.Controllers
 {
     public class CharacterController : Controller
@@ -12,9 +20,9 @@ namespace CharacterRoster.Web.Controllers
         public CharacterController ()
         {
             //Gets the CharacterDatabase connection string from the config file
-            var connString = ConfigurationManager.ConnectionStrings["MemoryCharacterDatabase"].ConnectionString;
+            var connString = ConfigurationManager.ConnectionStrings["CharacterDatabase"].ConnectionString;
 
-           //var _database = new MemoryCharacterDatabase.MemoryCharacterDatabase(connString);
+           var _database = new MemoryCharacterDatabase(connString);
         }
 
         // GET: Character
---
+++ labs/Lab5/CharacterRoster.Web/Controllers/MemoryCharacterDatabase.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * ITSE 1430
+ * Donald Helaire
+ * Lab5
+ */
+
+namespace CharacterRoster.Web.Controllers
+{
+    internal class MemoryCharacterDatabase
+    {
+        private string connString;
+
+        public MemoryCharacterDatabase ( string connString )
+        {
+            this.connString=connString;
+        }
+    }
+}
